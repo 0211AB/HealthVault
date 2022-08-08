8a2d9b5e65a46de104489c87c506ad999cb44bfc
0211AB <abhaybajajit@gmail.com>
2022-08-09 01:45:27 +0530

    Production v1.2.2

---
+++ frontend/src/Store/emailKey.js
@@ -0,0 +1,5 @@
+module.exports = {
+    REACT_APP_EMAILJS_SERVICE_ID: "service_1vb513w",
+    REACT_APP_EMAILJS_TEMPLATE_ID: "template_y7y4xzj",
+    REACT_APP_EMAILJS_USER_ID: "JvQPc1tjcKcutJuvw"
+}

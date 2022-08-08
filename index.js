49e7de4d8ea0162be2999e2f4cf9df0f5e9b7bf8
0211AB <abhaybajajit@gmail.com>
2022-08-09 01:21:30 +0530

    Production v1.1

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 8000;
 const patientRoutes = require("./routes/patient");
 const doctorRoutes = require("./routes/doctor");
 const prescriptionRoutes = require("./routes/prescription");
-const appRoutes = require("./routes/appointments")
-const imageRoutes = require("./routes/image")
+const appRoutes = require("./routes/appointments");
+const imageRoutes = require("./routes/image");
 
 const URI = `mongodb+srv://admin:${process.env.DB_PASS}@cluster01.5gpna.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -28,12 +28,22 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static('images'))
+app.use(express.static("images"));
 app.use(patientRoutes);
 app.use(doctorRoutes);
 app.use(prescriptionRoutes);
 app.use(appRoutes);
-app.use(imageRoutes)
+app.use(imageRoutes);
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("App is running on port", port);

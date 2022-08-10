1f8cce0de51e34abbf4ead88bc1681e31caa3f74
0211AB <abhaybajajit@gmail.com>
2022-08-10 22:13:22 +0530

    Changed files to make production ready

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging"
 ) {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
   });
 }
 
